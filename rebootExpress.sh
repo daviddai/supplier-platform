1fa60c7368c08f22a55c3658ea8fffdfd96c250c
daviddai <david.dai.personal@gmail.com>
2020-01-01 21:58:18 +1100

    sync job wip

---
+++ rebootExpress.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pkill -f node
+nohup node /usr/renderer/server.js &
---
+++ syncJob.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+build_express_folder="./build/express";
+express_folder="./src/main/javascript/build/server/server.js";
+
+# sync express renderer service
+if test -f "$express_folder"; then
+    cp ${express_folder} ${build_express_folder}
+    echo "server.js has been updated";
+    echo "Now try to restart rendering server...";
+    docker exec supplier-platform /usr/renderer/rebootExpress.sh
+    echo "Rendering server restarted";
+else
+    echo "server.js does not exist"
+fi
